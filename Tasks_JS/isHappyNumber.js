a9e5397c3d6af6322d58486c0ec52802ee17248b
Andrew Stepanov <an.step88@gmail.com>
2022-04-20 14:03:15 +0300

    Fix tasks

--- Tasks_JS/isHappyNumber.js
+++ Tasks_JS/isHappyNumber.js
@@ -1,17 +1,17 @@
 //Функция, которая должна вернуть "true", если число счастливое (такое, которое в результате ряда преобразований вида "сумма квадратов цифр" превратится в единицу), и "false", если нет (количество итераций процесса поиска необходимо ограничить числом 10)
 
-const sumOfSquareDigits = num => {
+const sumOfSquareDigits = (num) => {
   const str = String(num);
   let result = 0;
   let i = 0;
   while (i < str.length) {
     result += Number(str[i]) ** 2;
-    i +=1;
+    i += 1;
   }
   return result;
 };
 
-const isHappyNumber = num => {
+const isHappyNumber = (num) => {
   for (let i = 0; i < 10; i += 1) {
     if (num === 1) {
       return true;
