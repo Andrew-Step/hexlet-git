5fc5b56db90bd47ff9b6faac7292caad45d5b352
Andrew Stepanov <an.step88@gmail.com>
2022-11-21 20:57:25 +0300

    Modified file from Tasks_JS

--- Tasks_JS/isHappyTicket.js
+++ Tasks_JS/isHappyTicket.js
@@ -1,12 +1,19 @@
 //Функция, которая проверяет является ли билет счастливым (номер — всегда строка). Функция должна возвращать "true", если билет счастливый, или "false", если нет
 
-const isHappyTicket = str => {
-    let result = 0;
-  
-    for (let i = 0; i < str.length / 2; i += 1) {
-      result += str[i] - str[str.length - (1 + i)];
-    }
-    return result === 0;
-  };
-console.log(isHappyTicket('2222212340')); //TRUE
+const isHappyTicket = (str) => {
+  let len = str.length;
+  let result = 0;
+
+  if (len % 2 !== 0) {
+    return false;
+  }
+
+  for (let i = 0; i < len / 2; i += 1) {
+    result += str[i] - str[len - (i + 1)];
+  }
+
+  return result === 0;
+};
+
+console.log(isHappyTicket("385916")); //TRUE
 //export default isHappyTicket;
